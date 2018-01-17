9dbabc1b3906bcc8e2c1bdbe381faf6b1e8a96cc
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-16 16:46:35 -0800

    chore: prettier

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 'use strict'
 
 let prompt = require('./index')
-prompt('hide: ', {method: 'hide'})
-.then(password => {
-  console.log(`entered: ${password}`)
-  return prompt('notty: ', {method: 'notty'})
-}).then(password => {
-  console.log(`entered: ${password}`)
-  return prompt('mask: ', {method: 'mask'})
-}).then(password => {
-  console.log(`entered: ${password}`)
-})
-.catch(err => console.error(err.stack))
+prompt('hide: ', { method: 'hide' })
+  .then(password => {
+    console.log(`entered: ${password}`)
+    return prompt('notty: ', { method: 'notty' })
+  })
+  .then(password => {
+    console.log(`entered: ${password}`)
+    return prompt('mask: ', { method: 'mask' })
+  })
+  .then(password => {
+    console.log(`entered: ${password}`)
+  })
+  .catch(err => console.error(err.stack))
